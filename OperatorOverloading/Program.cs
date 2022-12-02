45557570177e2052388a238d129e6746aa0afea6
Студент1 <1@DESKTOP-7RO4MU5>
2022-12-02 09:15:53 +0300

    Добавьте файлы проекта.

---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,49 @@
+﻿using OperatorOverloading;
+Console.WriteLine();
+Console.WriteLine("1. Dollar -> roubles");
+Console.WriteLine("2. Перегрузки операторов");
+Console.WriteLine();
+string menu = Console.ReadLine();
+bool parse = int.TryParse(menu, out var select);
+if (parse)
+{
+    if (select == 1)
+    {
+        Console.WriteLine("Сколько долларов");
+        string summ = Console.ReadLine();
+        bool parse1 = decimal.TryParse(summ, out var dollars);
+        if (parse1)
+        {
+            Dollar dollar = new() { Summ = dollars };
+            Roubles roubles = (Roubles)dollar;
+            Console.WriteLine($"В рублях - {roubles.Summ}");
+        }
+        return;
+    }
+    else if (select == 2)
+    {
+        Overloads();
+        return;
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Ошибка");
+        Console.WriteLine();
+    }
+}
+void Overloads()
+{
+    Console.WriteLine($"Введите первое число");
+    string number1 = Console.ReadLine();
+    bool check1 = int.TryParse(number1, out var result1);
+    Console.WriteLine($"Введите второе число");
+    string number2 = Console.ReadLine();
+    bool check2 = int.TryParse(number2, out var result2);
+    var a = new MyNumber { Value = result1 };
+    var b = new MyNumber { Value = result2 };
+    Console.WriteLine($"Сложение {a.Value + b.Value}");
+    Console.WriteLine($"Умножение{a.Value * b.Value}");
+    Console.WriteLine($"Вычетание{a.Value - b.Value}");
+    Console.WriteLine($"Деление{a.Value / b.Value}");
+}
---
+++ OperatorOverloading/Roubles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading;
+
+public class Roubles
+{
+    public decimal Summ { get; set; }
+    public static explicit operator Roubles(Dollar dollar)
+    {
+        decimal summ = dollar.Summ * (decimal)70.45;
+        return new Roubles() { Summ = summ };
+    }
+}
